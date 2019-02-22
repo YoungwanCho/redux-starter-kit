7eb745398ca426921fa284bc74a40d2ab3840715
developer119 <developer119korea@gmail.com>
2019-02-22 19:47:31 +0900

    요청 Promise를 취소해 봄
    크롬에서 이스케이프키 이벤트를 제대로 받지 못해서 버튼으로 따로 처리함

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import * as postActions from './modules/post';
 
 
 class App extends Component {
+    cancelRequest = null
+
+    handleCancel = () => {
+        if(this.cancelRequest) {
+            this.cancelRequest();
+            this.cancelRequest = null;
+        }
+    }
+
     loadData = async () => {
         const { PostActions, number } = this.props;
         try {
-            const response = await PostActions.getPost(number);
+            const p = PostActions.getPost(number);
+            this.cancelRequest = p.cancel;
+            const response = await p;
             console.log(response);
         } catch(e) {
             console.log(e);
@@ -18,6 +29,12 @@ class App extends Component {
 
     componentDidMount() {
         this.loadData();
+        window.addEventListener('keyup', (e) => {
+            console.log(e.key);
+            if(e.key === 'Escape') {
+                this.handleCancel();
+            }
+        })
     }
 
     componentDidUpdate(prevProps, prevState) {
@@ -31,6 +48,9 @@ class App extends Component {
 
         return (
             <div>
+                <button onClick={CounterActions.increment}>+</button>
+                <button onClick={CounterActions.decrement}>-</button>
+                <button onClick={this.handleCancel}>취소</button>
                 <h1>{number}</h1>
                 {
                     loading
@@ -46,8 +66,6 @@ class App extends Component {
                         )
                     )
                 }
-                <button onClick={CounterActions.increment}>+</button>
-                <button onClick={CounterActions.decrement}>-</button>
             </div>
         );
     }
